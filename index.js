6a135ca2438a6aef7af21138a357d5ac48a6f655
toure5013 <toure5013@gmail.com>
2020-03-24 16:51:25 +0000

    TEXT TO MP3 EN FRANCAIS

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const gTTS = require('gtts');
 
 var messageATransformer = 'je suis SOULEYMANE'
+var date = new Date()
 var langagueMessage = 'fr'
 var gtts = new gTTS( messageATransformer , langagueMessage);
-gtts.save('./hello.mp3', function (err, result) {
+gtts.save('./' + date + 'hello.mp3', function (err, result) {
   if(err) { throw new Error(err) }
   console.log('Success! Open file /tmp/hello.mp3 to hear result.');
 });
